d91eb725b74916ae91a247125769e6aad94ba134
Peter Moss <petermoss@Peters-MacBook.local>
2026-03-17 13:27:10 +0800

    fix: root layout wrapper needs h-screen overflow-hidden to contain child layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <AuthProvider>
           <AppCacheProvider>
-            <div className="relative z-10">{children}</div>
+            <div className="relative z-10 h-screen overflow-hidden">{children}</div>
             <InstallPwaPrompt />
           </AppCacheProvider>
           </AuthProvider>
