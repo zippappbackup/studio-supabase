64ffe6102056d12fa631c05038e4bc976d6e3e8c
Peter Moss <petermoss@Peters-MacBook.local>
2026-03-19 22:42:38 +0800

    fix: remove h-screen overflow-hidden from root layout div causing header to scroll away

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <AuthProvider>
           <AppCacheProvider>
-            <div className="relative z-10 h-screen overflow-hidden">{children}</div>
+            <div className="relative z-10">{children}</div>
             <InstallPwaPrompt />
           </AppCacheProvider>
           </AuthProvider>
